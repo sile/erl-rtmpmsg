9c56c0d3e23c7e4a1f95a9a95a9cc0db77d43e28
Takeru Ohta <phjgt308@gmail.com>
2013-04-11 11:27:00 +0900

    メッセージエンコーダー追加

---
+++ src/rtmpmsg_encoder.erl
@@ -0,0 +1,24 @@
+-module(rtmpmsg_encoder).
+-export([new/0, encode/3]).
+
+-include("../include/rtmpmsg.hrl").
+
+-define(STATE, ?MODULE).
+
+-record(?STATE,
+        {
+          chunk_enc = rtmpmsg_chunk_encode:init() :: term() % XXX
+        }).
+
+new() ->
+    #?STATE{}.
+
+encode(State, ChunkStreamId, Msg) ->
+    Enc0 = State#?STATE.chunk_enc,
+    Chunk = rtmpmsg_message_encode:encode_to_chunk(ChunkStreamId, Msg),
+    {Enc1, EncodedData} = rtmpmsg_chunk_encode:encode(Enc0, Chunk),
+    Enc2 = case Msg of
+               #rtmpmsg_set_chunk_size{size=Size} -> rtmpmsg_chunk_encode:set_chunk_size(Enc1, Size);
+               _                                  -> Enc1
+           end,
+    {State#?STATE{chunk_enc=Enc2}, EncodedData}.
