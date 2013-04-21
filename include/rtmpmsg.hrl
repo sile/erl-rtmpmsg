173a160937cb5da922d347ecf5e893c35f03295b
Takeru Ohta <phjgt308@gmail.com>
2013-04-21 18:20:46 +0900

    レコードの型宣言を若干正確なものに修正

--- include/rtmpmsg.hrl
+++ include/rtmpmsg.hrl
@@ -1,16 +1,16 @@
 
 -record(rtmpmsg_unknown,
         {
-          type_id = 0      :: non_neg_integer(),
+          type_id = 0      :: rtmpmsg:message_type_id(),
           payload = <<"">> :: binary()
         }).
 
 -record(rtmpmsg,
         {
-          stream_id = 0 :: non_neg_integer(),
-          type_id = 0 :: non_neg_integer(),
-          timestamp = 0 :: non_neg_integer(),
-          body :: term() %% TODO
+          stream_id = 0 :: rtmpmsg:message_stream_id(),
+          type_id = 0   :: rtmpmsg:message_type_id(),
+          timestamp = 0 :: rtmpmsg:message_timestamp(),
+          body          :: rtmpmsg:message_body()
         }).
 
 
@@ -21,13 +21,13 @@
 
 -record(rtmpmsg_set_peer_bandwidth,
         {
-          size = 0 :: non_neg_integer(),
-          limit_type = hard :: hard|soft|dynamic
+          size = 0          :: non_neg_integer(),
+          limit_type = hard :: rtmpmsg:set_peer_bandwidth_limit_type()
         }).
 
 -record(rtmpmsg_abort,
         {
-          chunk_stream_id = 0 :: non_neg_integer() % TODO
+          chunk_stream_id = 0 :: rtmpmsg:chunk_stream_id()
         }).
 
 -record(rtmpmsg_ack,
@@ -42,10 +42,9 @@
 
 -record(rtmpmsg_user_control,
         {
-          event :: term() % TODO
+          event :: rtmpmsg:user_control_event()
         }).
 
-%% TODO
 -record(rtmpmsg_shared_object,
         {
           amf_version = amf0 :: amf:amf_version(),
@@ -79,7 +78,7 @@
 
 -record(rtmpmsg_aggregate,
         {
-          messages = [] :: list() % TODO
+          messages = [] :: [rtmpmsg:message()]
         }).
 
 -record(rtmpmsg_event_unknown,
@@ -90,46 +89,46 @@
 
 -record(rtmpmsg_event_stream_begin,
         {
-          stream_id = 0 :: non_neg_integer()
+          stream_id = 0 :: rtmpmsg:message_stream_id()
         }).
 
 -record(rtmpmsg_event_stream_eof,
         {
-          stream_id = 0 :: non_neg_integer()
+          stream_id = 0 :: rtmpmsg:message_stream_id()
         }).
 
 -record(rtmpmsg_event_stream_dry,
         {
-          stream_id = 0 :: non_neg_integer()
+          stream_id = 0 :: rtmpmsg:message_stream_id()
         }).
 
 -record(rtmpmsg_event_stream_is_recorded,
         {
-          stream_id = 0 :: non_neg_integer()
+          stream_id = 0 :: rtmpmsg:message_stream_id()
         }).
 
 -record(rtmpmsg_event_buffer_empty,
         {
-          stream_id = 0 :: non_neg_integer()
+          stream_id = 0 :: rtmpmsg:message_stream_id()
         }).
 
 -record(rtmpmsg_event_buffer_ready,
         {
-          stream_id = 0 :: non_neg_integer()
+          stream_id = 0 :: rtmpmsg:message_stream_id()
         }).
 
 -record(rtmpmsg_event_set_buffer_length,
         {
-          stream_id = 0 :: non_neg_integer(),
-          length = 0 :: non_neg_integer()
+          stream_id = 0 :: rtmpmsg:message_stream_id(),
+          length = 0    :: rtmpmsg:milliseconds()
         }).
 
 -record(rtmpmsg_event_ping_request,
         {
-          timestamp = 0 :: non_neg_integer()
+          timestamp = 0 :: rtmpmsg:message_timestamp()
         }).
 
 -record(rtmpmsg_event_ping_response,
         {
-          timestamp = 0 :: non_neg_integer()
+          timestamp = 0 :: rtmpmsg:message_timestamp()
         }).
