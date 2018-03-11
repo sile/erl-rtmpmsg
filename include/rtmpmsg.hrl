c37eecb204069cbb12b5b2051d494cdbeeb44a28
Takeru Ohta <phjgt308@gmail.com>
2018-03-11 19:48:48 +0900

    Add `RTMPMSG_` prefix for exported macros

--- include/rtmpmsg.hrl
+++ include/rtmpmsg.hrl
@@ -134,29 +134,29 @@
         }).
 
 %% Protocol Control Message
--define(TYPE_SET_CHUNK_SIZE,     1).
--define(TYPE_ABORT,              2).
--define(TYPE_ACK,                3).
--define(TYPE_USER_CONTROL,       4).
--define(TYPE_WIN_ACK_SIZE,       5).
--define(TYPE_SET_PEER_BANDWIDTH, 6).
+-define(RTMPMSGP_TYPE_SET_CHUNK_SIZE,     1).
+-define(RTMPMSGP_TYPE_ABORT,              2).
+-define(RTMPMSGP_TYPE_ACK,                3).
+-define(RTMPMSGP_TYPE_USER_CONTROL,       4).
+-define(RTMPMSGP_TYPE_WIN_ACK_SIZE,       5).
+-define(RTMPMSGP_TYPE_SET_PEER_BANDWIDTH, 6).
 
--define(TYPE_AUDIO,              8).
--define(TYPE_VIDEO,              9).
--define(TYPE_DATA_AMF3,          15).
--define(TYPE_SHARED_OBJECT_AMF3, 16). % aka. kMsgContainerEx
--define(TYPE_COMMAND_AMF3,       17).
--define(TYPE_DATA_AMF0,          18).
--define(TYPE_SHARED_OBJECT_AMF0, 19). % aka. kMsgContainer
--define(TYPE_COMMAND_AMF0,       20).
--define(TYPE_AGGREGATE,          22).
+-define(RTMPMSGP_TYPE_AUDIO,              8).
+-define(RTMPMSGP_TYPE_VIDEO,              9).
+-define(RTMPMSGP_TYPE_DATA_AMF3,          15).
+-define(RTMPMSGP_TYPE_SHARED_OBJECT_AMF3, 16). % aka. kMsgContainerEx
+-define(RTMPMSGP_TYPE_COMMAND_AMF3,       17).
+-define(RTMPMSGP_TYPE_DATA_AMF0,          18).
+-define(RTMPMSGP_TYPE_SHARED_OBJECT_AMF0, 19). % aka. kMsgContainer
+-define(RTMPMSGP_TYPE_COMMAND_AMF0,       20).
+-define(RTMPMSGP_TYPE_AGGREGATE,          22).
 
--define(EVENT_STREAM_BEGIN, 0).
--define(EVENT_STREAM_EOF, 1).
--define(EVENT_STREAM_DRY, 2).
--define(EVENT_SET_BUFFER_LENGTH, 3).
--define(EVENT_STREAM_IS_RECORDED, 4).
--define(EVENT_PING_REQUEST, 6).
--define(EVENT_PING_RESPONSE, 7).
--define(EVENT_BUFFER_EMPTY, 31).
--define(EVENT_BUFFER_READY, 32).
+-define(RTMPMSG_EVENT_STREAM_BEGIN, 0).
+-define(RTMPMSG_EVENT_STREAM_EOF, 1).
+-define(RTMPMSG_EVENT_STREAM_DRY, 2).
+-define(RTMPMSG_EVENT_SET_BUFFER_LENGTH, 3).
+-define(RTMPMSG_EVENT_STREAM_IS_RECORDED, 4).
+-define(RTMPMSG_EVENT_PING_REQUEST, 6).
+-define(RTMPMSG_EVENT_PING_RESPONSE, 7).
+-define(RTMPMSG_EVENT_BUFFER_EMPTY, 31).
+-define(RTMPMSG_EVENT_BUFFER_READY, 32).
